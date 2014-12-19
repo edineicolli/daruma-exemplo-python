fcad77800de77f5b99e5db8e9fb17eed80f12186
Edinei Colli <edismo@gmail.com>
2014-12-18 22:59:07 -0200

    adicionado FISCAL_confCFBPProgramarUF

--- scripts/fiscal/ui_fiscal_confcfbpprogramaruf.py
+++ scripts/fiscal/ui_fiscal_confcfbpprogramaruf.py
@@ -8,6 +8,9 @@
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
+from pydaruma.pydaruma import confCFBPProgramarUF_ECF_Daruma
+from scripts.fiscal.retornofiscal import tratarRetornoFiscal
+
 
 class Ui_ui_FISCAL_confCFBPProgramarUF(QtGui.QWidget):
 
@@ -15,6 +18,18 @@ class Ui_ui_FISCAL_confCFBPProgramarUF(QtGui.QWidget):
         super(Ui_ui_FISCAL_confCFBPProgramarUF, self).__init__()
 
         self.setupUi(self)
+        self.pushButtonEnviar.clicked.connect(self.on_pushButtonEnviar_clicked)
+        self.pushButtonCancelar.clicked.connect(self.on_pushButtonCancelar_clicked)
+
+    def on_pushButtonEnviar_clicked(self):
+        # Recebe valor em QString do LineEdit
+        StrUF = self.lineEditUF.text();
+
+        # Chamada do Método
+        tratarRetornoFiscal(confCFBPProgramarUF_ECF_Daruma(StrUF), self);
+
+    def on_pushButtonCancelar_clicked(self):
+        self.close()
 
     def setupUi(self, ui_FISCAL_confCFBPProgramarUF):
         ui_FISCAL_confCFBPProgramarUF.setObjectName("ui_FISCAL_confCFBPProgramarUF")
