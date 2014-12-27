b34818528e8c7f55e397b0d2f2702f1721fc4a80
Edinei Colli <edismo@gmail.com>
2014-12-27 16:48:43 -0200

    adicionado FISCAL_eRSAAssinarArquivo

--- scripts/fiscal/ui_fiscal_ersaassinararquivo.py
+++ scripts/fiscal/ui_fiscal_ersaassinararquivo.py
@@ -8,6 +8,9 @@
 # WARNING! All changes made in this file will be lost!
 
 from PySide import QtCore, QtGui
+from pydaruma.pydaruma import eRSAAssinarArquivo_ECF_Daruma
+from scripts.fiscal.retornofiscal import tratarRetornoFiscal
+
 
 class Ui_ui_FISCAL_eRSAAssinarArquivo(QtGui.QWidget):
 
@@ -15,6 +18,18 @@ class Ui_ui_FISCAL_eRSAAssinarArquivo(QtGui.QWidget):
         super(Ui_ui_FISCAL_eRSAAssinarArquivo, self).__init__()
 
         self.setupUi(self)
+        self.pushButtonEnviar.clicked.connect(self.on_pushButtonEnviar_clicked)
+        self.pushButtonCancelar.clicked.connect(self.on_pushButtonCancelar_clicked)
+
+    def on_pushButtonEnviar_clicked(self):
+
+        StrCaminhoArq = self.lineEditCaminhoArq.text()
+        StrCaminhoKey = self.lineEditCaminhoKey.text()
+
+        tratarRetornoFiscal(eRSAAssinarArquivo_ECF_Daruma(StrCaminhoArq,StrCaminhoKey), self)
+
+    def on_pushButtonCancelar_clicked(self):
+        self.close()
 
     def setupUi(self, ui_FISCAL_eRSAAssinarArquivo):
         ui_FISCAL_eRSAAssinarArquivo.setObjectName("ui_FISCAL_eRSAAssinarArquivo")
