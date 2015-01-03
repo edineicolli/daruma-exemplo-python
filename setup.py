8e67c18814a97639448a1ba26ef986ef4690030b
Edinei Colli <edismo@gmail.com>
2015-01-03 19:24:16 -0200

    ajustado geracao do binario

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = None
+targetName = None
 if sys.platform == "win32":
     base = "Win32GUI"
+    targetName = "DarumaFramework_Python_Qt.exe"
+else:
+    targetName = "darumaframework_python_qt"
 
-setup(  name = "exemplodaruma",
+setup(  name = "darumaframework_python_qt",
         version = "1.0",
         description = "Exemplo Daruma Multiplataforma em Python",
         options = {"build_exe": build_exe_options},
         executables = [Executable("main.py",
                                   base=base,
-                                  targetName = "ExemploDarumaPython.exe",)])
+                                  targetName = targetName,)])
